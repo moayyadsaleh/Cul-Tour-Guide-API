f455ed65350622f3e7b78fdb0d3856f2c2718d7a
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-15 12:35:21 -0600

    Santize and validate input

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import express from "express";
 import mongoose from 'mongoose';
 import rateLimit from 'express-rate-limit';
+import { body, validationResult } from 'express-validator';
+
 
 // Apply rate limiting middleware to specific routes
 const apiLimiter = rateLimit({
@@ -9,6 +11,24 @@ const apiLimiter = rateLimit({
     max: 100, // Max requests per window
     message: 'Too many requests from this IP, please try again later.',
   });
+
+
+// Validation and sanitization middleware
+const validateAndSanitizeInput = [
+    // Validate and sanitize input fields using express-validator methods
+    body('culturalFact').trim().isString().escape(),
+    body('country').trim().isString().escape(),
+  
+    // Handle validation errors
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    }
+  ]; 
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -56,132 +76,171 @@ const culturalFactSchema = new mongoose.Schema({
 //////////////////////////// Requests Targeting all culturalFacts
 
 //Display all cultural facts
-app.get('/cultural-facts',apiLimiter, async (req, res) => {
-    try {
-      const foundCulturalFacts = await CulturalFact.find();
-      console.log(foundCulturalFacts, "Cultural facts are successfully fetched");
-      res.send(foundCulturalFacts); 
-    } catch (error) {
-      console.error('Error retrieving cultural facts:', error);
-      res.status(500).json({ error: 'An error occurred while retrieving cultural facts.' });
+app.get(
+    '/cultural-facts',
+    validateAndSanitizeInput, // Apply validation and sanitization 
+    apiLimiter, // Then apply rate limiting
+    async (req, res) => {
+      try {
+        const foundCulturalFacts = await CulturalFact.find();
+        console.log(foundCulturalFacts, 'Cultural facts are successfully fetched');
+        res.send(foundCulturalFacts);
+      } catch (error) {
+        console.error('Error retrieving cultural facts:', error);
+        res.status(500).json({ error: 'An error occurred while retrieving cultural facts.' });
+      }
     }
-  });
-
+  );
   //Add new cultural fact
-  app.post('/cultural-facts', apiLimiter, async (req, res) => {
-    try {
-      console.log(req.body.culturalFact);
-      console.log(req.body.country);
-      const newCulturalFact = new CulturalFact({
-        culturalFact: req.body.culturalFact,
-        country: req.body.country
-      });
-      newCulturalFact.save()
-        .then(savedCulturalFact => {
-          console.log("Cultural fact is successfully added", savedCulturalFact);
-          res.status(201).json(savedCulturalFact);
-        })
-        .catch(error => {
-          console.error("Error adding cultural fact:", error);
-          res.status(500).json({ error: "An error occurred while adding the cultural fact." });
+  app.post(
+    '/cultural-facts',
+    validateAndSanitizeInput, // Apply validation and sanitization first
+    apiLimiter, // Then apply rate limiting
+    async (req, res) => {
+      try {
+        const newCulturalFact = new CulturalFact({
+          culturalFact: req.body.culturalFact,
+          country: req.body.country
         });
-    } catch (error) {
-      console.error("Error processing request:", error);
-      res.status(400).json({ error: "Bad request." });
+        newCulturalFact.save()
+          .then(savedCulturalFact => {
+            console.log('Cultural fact is successfully added', savedCulturalFact);
+            res.status(201).json(savedCulturalFact);
+          })
+          .catch(error => {
+            console.error('Error adding cultural fact:', error);
+            res.status(500).json({ error: 'An error occurred while adding the cultural fact.' });
+          });
+      } catch (error) {
+        console.error('Error processing request:', error);
+        res.status(400).json({ error: 'Bad request.' });
+      }
     }
-  });
+  );
 
   //delete all cultural facts. This is not going to be used in the documentation
-  app.delete("/cultural-facts", apiLimiter, async (req, res) => {
-    try {
-      const deletedCulturalFacts = await CulturalFact.deleteMany();
-      console.log(deletedCulturalFacts, "All articles are successfully deleted");
-      res.json(deletedCulturalFacts);
-    } catch (error) {
-      console.error("Error deleting cultural facts", error);
-      res.status(500).send("Error deleting articles");
+  app.delete(
+    "/cultural-facts",
+    validateAndSanitizeInput, // Apply validation and sanitization first
+    apiLimiter, // Then apply rate limiting
+    async (req, res) => {
+      try {
+        const deletedCulturalFacts = await CulturalFact.deleteMany();
+        console.log(deletedCulturalFacts, "All articles are successfully deleted");
+        res.json(deletedCulturalFacts);
+      } catch (error) {
+        console.error("Error deleting cultural facts", error);
+        res.status(500).send("Error deleting articles");
+      }
     }
-  });
+  );
 
 
 
 ////////////////////////////Requests Targeting individual culturalFacts
-app.get("/cultural-facts/:id",apiLimiter, async (req, res) => {
-    try {
-      const factId = req.params.id;
-  
-      const foundFact = await CulturalFact.findById(factId);
-  
-      if (foundFact) {
-        res.send(foundFact); // 
-      } else {
-        res.status(404).send({ error: 'Cultural fact not found.' });
+app.get(
+    "/cultural-facts/:id",
+    validateAndSanitizeInput, // Apply validation and sanitization first
+    apiLimiter, // Then apply rate limiting
+    async (req, res) => {
+      try {
+        const factId = req.params.id;
+    
+        const foundFact = await CulturalFact.findById(factId);
+    
+        if (foundFact) {
+          res.send(foundFact);
+        } else {
+          res.status(404).send({ error: 'Cultural fact not found.' });
+        }
+      } catch (error) {
+        res.status(500).json({ error: 'An error occurred while retrieving the cultural fact.' });
       }
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred while retrieving the cultural fact.' });
     }
-  });
+  );
 
   //Request for updating one cultural fact
-  app.put("/cultural-facts/:id",apiLimiter, async (req, res) => {
-    try {
-      const factId = req.params.id;
-  
-      // Update both the cultural fact content and country
-      const updatedFact = await CulturalFact.findByIdAndUpdate(factId, {
-        culturalFact: req.body.culturalFact,
-        country: req.body.country,
-      }, { new: true }); // 
-  
-      if (updatedFact) {
-        res.json(updatedFact);
-      } else {
-        res.status(404).json({ error: 'Cultural fact not found.'});
+  app.put(
+    "/cultural-facts/:id",
+    validateAndSanitizeInput, // Apply validation and sanitization first
+    apiLimiter, // Then apply rate limiting
+    async (req, res) => {
+      try {
+        const factId = req.params.id;
+    
+        // Update both the cultural fact content and country
+        const updatedFact = await CulturalFact.findByIdAndUpdate(
+          factId,
+          {
+            culturalFact: req.body.culturalFact,
+            country: req.body.country,
+          },
+          { new: true }
+        );
+    
+        if (updatedFact) {
+          res.json(updatedFact);
+        } else {
+          res.status(404).json({ error: 'Cultural fact not found.'});
+        }
+      } catch (error) {
+        res.status(500).json({ error: 'An error occurred while updating the cultural fact.' });
       }
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred while updating the cultural fact.' });
     }
-  });
+  );
+  
 
 
-  app.patch("/cultural-facts/:id",apiLimiter, async (req, res) => {
-    try {
-      const factId = req.params.id;
-  
-      // Update specific fields of the cultural fact (culturalFact and/or country)
-      const updatedFact = await CulturalFact.findByIdAndUpdate(
-        factId,
-        { $set: req.body }, //  $set  updates only specified fields
-        { new: true }
-      );
-  
-      if (updatedFact) {
-        res.send(updatedFact);
-      } else {
-        res.status(404).send({ error: 'Cultural fact not found.' });
+  app.patch(
+    "/cultural-facts/:id",
+    validateAndSanitizeInput, // Apply validation and sanitization first
+    apiLimiter, // Then apply rate limiting
+    async (req, res) => {
+      try {
+        const factId = req.params.id;
+    
+        // Update specific fields of the cultural fact (culturalFact and/or country)
+        const updatedFact = await CulturalFact.findByIdAndUpdate(
+          factId,
+          { $set: req.body }, //  $set  updates only specified fields
+          { new: true }
+        );
+    
+        if (updatedFact) {
+          res.send(updatedFact);
+        } else {
+          res.status(404).send({ error: 'Cultural fact not found.' });
+        }
+      } catch (error) {
+        res.status(500).send({ error: 'An error occurred while updating the cultural fact.' });
       }
-    } catch (error) {
-      res.status(500).send({ error: 'An error occurred while updating the cultural fact.' });
     }
-  });
+  );
+  
 
 
-  app.delete("/cultural-facts/:id", apiLimiter, async (req, res) => {
-    try {
-      const factId = req.params.id;
-  
-      // Find and delete the cultural fact based on its ID
-      const deletedFact = await CulturalFact.findByIdAndDelete(factId);
-  
-      if (deletedFact) {
-        res.json({ message: 'Cultural fact deleted successfully.' });
-      } else {
-        res.status(404).json({ error: 'Cultural fact not found.' });
+  app.delete(
+    "/cultural-facts/:id",
+    validateAndSanitizeInput, // Apply validation and sanitization first
+    apiLimiter, // Then apply rate limiting
+    async (req, res) => {
+      try {
+        const factId = req.params.id;
+    
+        // Find and delete the cultural fact based on its ID
+        const deletedFact = await CulturalFact.findByIdAndDelete(factId);
+    
+        if (deletedFact) {
+          res.json({ message: 'Cultural fact deleted successfully.' });
+        } else {
+          res.status(404).json({ error: 'Cultural fact not found.' });
+        }
+      } catch (error) {
+        res.status(500).json({ error: 'An error occurred while deleting the cultural fact.' });
       }
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred while deleting the cultural fact.' });
     }
-  });
+  );
+  
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
